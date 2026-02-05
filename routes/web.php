7bbcf98730b87a0845ff7445ac18427e2a9f563f
shoya <shokosushoya@outlook.jp>
2026-02-05 21:34:06 +0900

    initial commit

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
-
-Route::get('/index',[PostsController::class,'index']);
-Route::get('/show', [PostsController::class,'show']);
