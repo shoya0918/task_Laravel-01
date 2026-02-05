96f639648e24f43054a1b90cda62fc09eba49c56
shoya <shokosushoya@outlook.jp>
2026-02-05 21:36:44 +0900

    create route & controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function show()
+    {
+        $title = '詳細画面';
+
+        return view('show',['title' => $title]);
+    }
+}
