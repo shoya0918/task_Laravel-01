9cfbaf23377bd887b657bc20a73f8c93bddd5061
shoya <shokosushoya@outlook.jp>
2026-03-04 19:12:46 +0900

    指摘箇所の修正

--- app/Http/Controllers/RemotePostController.php
+++ app/Http/Controllers/RemotePostController.php
@@ -10,7 +10,13 @@ class RemotePostController extends Controller
     public function store(Request $request)
     {
        try{
-            $post = Post::create($request->all());
+            $validated = $request->validate([
+                'title' => 'required|string|max:255',
+                'content' => 'required|string',
+                'author_id' => 'required|integer',
+            ])
+
+            $post = Post::create($validated);
             return response()->json([
                 'message' => 'success post',
                 'data' => $post
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\RemotePostController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/create-posts', [RemotePostController::class, 'store']);
+Route::post('/posts', [RemotePostController::class, 'store']);
